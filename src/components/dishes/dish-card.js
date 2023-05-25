858f40ae9cba350b53da2d390d797bf7bd8ca977
Tobias Ramos <tobiasramossilva@gmail.com>
2023-05-25 12:39:38 -0300

    Fazendo a estilização do componente dishCard

--- src/components/dishes/dish-card.js
+++ src/components/dishes/dish-card.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import hamburguer from '../img/hamburguer.png'
+import './dish-card.css'
 
 const DishCard = () => {
 
     return (
-        <div>
-            <img src={hamburguer} />
-            <h2>Oferta Picanha Cheddar Bacon</h2>
-            <p>Hambúrguer de picanha, molho de picanha, cebola crispy, bacon, queijo cheddar,
-                molho cheddar e pão mix de gergelim
-            </p>
-            <div>
-                <span>R$31,99</span>
-                <span>R$34,95</span>
+        <div className='container-dishCard'>
+            <img className='hamburguerImage' src={hamburguer} alt='Imagem do hamburguer' />
+            <div className='hamburgerDice'>
+                <h2 className='title'>Oferta Picanha Cheddar Bacon</h2>
+                <p className='description'>Hambúrguer de picanha, molho de picanha, cebola crispy, bacon, queijo cheddar,
+                    molho cheddar e pão mix de gergelim
+                </p>
+                <div className='price'>
+                    <span className='currentPrice'>R$31,99</span>
+                    <span className='oldPrice'>R$34,95</span>
+                </div>
             </div>
         </div>
     );
