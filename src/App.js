414cd67be5792b5b14979b215240bbe63ed05b8c
Tobias Ramos <tobiasramossilva@gmail.com>
2023-05-25 12:05:10 -0300

    Fazendo as estilizações do componente header

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import './App.css';
 import Header from './components/header/header';
-import DishCard from './components/dishes/dish-card';
-import DishForm from './components/dishes/dish-form';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <DishCard />
-      <DishForm />
-
     </div>
   );
 }
--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,14 +1,31 @@
 import { React } from "react";
+import logo from '../img/logo.png'
+import './header.css'
+import userImage from '../img/userCircle.png'
+import cartImage from '../img/shoppingCart.png'
 
 const Header = () => {
     return (
-        <div>
-            <img src={logo} />
-            <span>Entrega: R. Antonio Braune, 222</span>
-            <input placeholder="Busque por estabelecimento ou produtos" />
-            <img  /><span>ENTRAR</span>
-            <img  /><span>Carrinho</span>
-        </div>
+        <header className="header">
+            <img className="logo" src={logo} alt="Logo da empresa" />
+            <div className="delivery">
+                <span>Entrega:</span>
+                <p>Entrega: R. Antonio Braune, 222</p>
+            </div>
+            <div className="search">
+                <input type="text" placeholder="Busque por estabelecimento ou produtos" />
+            </div>
+            <div className="data">
+                <div className="userCircle">
+                    <img className="userImage" src={userImage} /><button>ENTRAR</button>
+                </div>
+                <div>
+                    <img className="cartImage" src={cartImage} /><button>Carrinho</button>
+                </div>
+
+
+            </div>
+        </header>
     );
 }
 
